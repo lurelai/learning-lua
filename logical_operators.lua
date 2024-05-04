7abffe414302e34b9acc24d457094e9dae44cdb1
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-03 22:18:48 -0300

    logical_operators.lua

---
+++ logical_operators.lua
@@ -0,0 +1,17 @@
+print(true and false) -- false
+print(false and true) -- false
+print(true and true) -- true
+print(false and false) -- false
+
+print("\n")
+
+print(false or false) -- false
+print(false or true) -- true
+print(true or false) -- true
+print(true or true) -- true
+
+print("\n")
+
+print(true == true) -- true
+print(true ~= true) -- false
+
