940af559a765553789fdeb745f6be7b718c48341
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-03 22:50:34 -0300

    conditionals

---
+++ conditionals.lua
@@ -0,0 +1,10 @@
+name = "Lucas"
+
+if (name == "Lucas") or (name == "Ariel") then
+	print("You're so ugly")
+elseif name == "Phillipe" then
+	print("You're the wrost thing in the world")
+else
+	print("Hmm, you're okay")
+end
+
