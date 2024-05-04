a193817c357e600fea90cc978661ace34d73c3c5
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-04 11:13:48 -0300

    table_constructor.lua

--- table_constructor.lua
+++ table_constructor.lua
@@ -5,6 +5,9 @@ test = {
 	{ 
 		otherTable = "other table" ,
 		"Array in other table"
+	},
+	namedTable = {
+		yeah = "It is possible too"
 	}
 }
 
@@ -12,4 +15,8 @@ print(test["name"])
 print(test[4])
 print(test[5]["otherTable"])
 print(test[5][1])
+print(test["namedTable"]["yeah"])
+
+test[10] = "Ariel"
+print(#test) -- It just show the first-step defined strings
 
