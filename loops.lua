97ac406a60bfd3c33efff053bd174728097608f1
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-04 21:36:07 -0300

    loops.lua

---
+++ loops.lua
@@ -0,0 +1,42 @@
+local loop1Count = 0
+
+while loop1Count < 10 do
+	print(loop1Count)
+	loop1Count = loop1Count + 1
+end
+
+
+
+local loop2Var = { "Lucas", "Ariel", "Oliveria" }
+local loop2Count = 1
+
+print("\n\nIt will stop if the condition is a nil")
+while loop2Var[loop2Count] do
+	print(loop2Count, loop2Var[loop2Count])
+	loop2Count = loop2Count + 1
+end
+
+while nil do
+	print("It won't get there")
+end
+
+
+
+local loop3 = 0
+
+print("\n")
+
+repeat 
+	print(loop3)
+	loop3 = loop3 + 1
+until not (loop3 < 10)
+
+
+
+print("\n")
+
+-- for ( variable, limit, increment or (-n) )
+for i=1000, 0, -500 do
+	print(i)
+end
+
