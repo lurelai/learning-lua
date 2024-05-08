b72a487ee88b701bb2482d622485067e3fe9af9f
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-05-08 19:49:21 -0300

    add the math method

---
+++ modules/math.lua
@@ -0,0 +1,20 @@
+mathMethods = {}
+
+mathMethods.plus = function(n1, n2)
+	return n1+n2
+end
+
+mathMethods.minus = function(n1, n2)
+	return n1-n2
+end
+
+mathMethods.times = function(n1, n2)
+	return n1*n2
+end
+
+mathMethods.dividedBy = function(n1, n2)
+	return n1 / n2
+end
+
+return mathMethods
+
